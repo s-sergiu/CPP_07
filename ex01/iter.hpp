34a9e9ba864a0460596a58af1f57b59af9df3034
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-08-21 13:44:11 +0200

    finished implementation of iter

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -1,5 +1,18 @@
 
-#ifdef ITER_HPP
+#ifndef ITER_HPP
 #define ITER_HPP
+
+#include <iostream>
+
+template<typename T_ARR>
+void iterate(T_ARR &arg) {
+	std::cout<<arg<<std::endl;
+}
+
+template<typename T_ARR, typename T_SIZE>
+void iter(T_ARR *arr, T_SIZE size, void(*func)(T_ARR&)) {
+	for (int i = 0; i < size; i++)
+		func(arr[i]);
+}
 
 #endif
